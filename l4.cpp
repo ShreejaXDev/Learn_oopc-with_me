3d565de18597533dad874b1657adb0724b726e2f
Shreeja Upadhyay <shreejaupadhyaycspitce@gmail.com>
2025-03-23 17:33:03 +0530

    Add files via upload

---
+++ l4.cpp
@@ -0,0 +1,20 @@
+//is character upper case or lower case
+
+#include<iostream>
+using namespace std;
+
+    int main()
+    {
+        char c;
+
+        cin>>c;
+
+        if(c>='a' && c<='z')
+            cout<<"lower case ";
+
+        else if(c>='A' && c<='Z')
+            cout<<"upper case ";
+
+        else
+            cout<<"enter proper character within a range";
+    }
