6e512012c8b242ffa1b11c9606a34f27cf5fad38
Shreeja Upadhyay <shreejaupadhyaycspitce@gmail.com>
2025-03-23 17:35:39 +0530

    Add files via upload

---
+++ l6.cpp
@@ -0,0 +1,32 @@
+//loops
+
+
+//print 1 to n
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout<<"enter n";
+    cin>>n;
+
+    for(int i=1 ; i<=n ; i++)
+    {
+        cout<<i<<endl;
+    }
+
+
+
+
+//using while
+int i=1;
+while(i<=n)
+{
+    cout<<i<<endl;
+
+    i++;
+}
+}
