fc21b3db6e4840b246507df719ad60eb1f610b57
Shreeja Upadhyay <shreejaupadhyaycspitce@gmail.com>
2025-03-23 17:28:43 +0530

    Add files via upload

---
+++ l1.cpp
@@ -0,0 +1,24 @@
+//is number is positive negative or zero
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+
+    cout<<"enter a number ";
+    cin>>n;
+
+    if(n>0)
+    {
+        cout<<"enterd number is positive";
+    }
+    else if(n<0)
+    {
+        cout<<"entered number is negatve";
+    }
+    else
+    {
+        cout<<"entered number is zero ";
+    }
+}
