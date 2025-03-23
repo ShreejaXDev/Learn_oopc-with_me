c3c973fde77d74b5cf8e26181388744a2af8a01f
Shreeja Upadhyay <shreejaupadhyaycspitce@gmail.com>
2025-03-23 17:46:09 +0530

    Add files via upload

---
+++ l8.cpp
@@ -0,0 +1,27 @@
+
+//check whether the number is prime or not?
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,c;
+
+    cin>>n;
+
+    for(int i=1 ; i<=n ; i++)
+    {
+        if(n%i==0)
+        {
+            c++;
+        }
+    }
+
+    if(c==2)
+    {
+        cout<<"given number is prime";
+    }
+    else
+        cout<<"give number is not prime ";
+}
