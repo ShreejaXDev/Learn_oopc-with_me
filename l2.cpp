1f67deb1f4dc1905abd09ab82bfce0d73d2fa07d
Shreeja Upadhyay <shreejaupadhyaycspitce@gmail.com>
2025-03-23 17:30:02 +0530

    Add files via upload

---
+++ l2.cpp
@@ -0,0 +1,22 @@
+//is person eligible to vote or not?
+
+#include<iostream>
+using namespace std;
+
+    int  main()
+    {
+        int a;
+
+        cout<<"enter age :";
+        cin>>a;
+
+        if(a>=18)
+
+        {
+            cout<<"you can vote ";
+        }
+        else
+        {
+            cout<<"you are under age so you cant vote ";
+        }
+    }
