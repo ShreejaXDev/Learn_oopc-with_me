5dc188a2248996cbcc945228488d197b60d37f51
Shreeja Upadhyay <shreejaupadhyaycspitce@gmail.com>
2025-03-23 17:32:07 +0530

    Add files via upload

---
+++ l3.cpp
@@ -0,0 +1,24 @@
+//odd or even number
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout<<"enter a number :";
+    cin>>n;
+
+    if(n%2==0)
+    {
+        cout<<"even";
+
+    }
+    else
+    {
+        cout<<"odd";
+    }
+
+
+}
