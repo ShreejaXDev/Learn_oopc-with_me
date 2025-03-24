7e211728fe5cddaf58338e5f8c662fea5be9ebe9
Shreeja Upadhyay <shreejaupadhyaycspitce@gmail.com>
2025-03-25 00:30:00 +0530

    Add files via upload

---
+++ l13.cpp
@@ -0,0 +1,16 @@
+
+// function for add to numbers
+
+#include<iostream>
+using namespace std;
+
+int sum(int a, int b)
+{
+    return a+b;
+}
+int main()
+{
+    int result;
+    result=sum(5,10);
+    cout<<result;
+}
