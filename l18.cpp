54491cf2ac490a9d8bce84b6ded7d0b9449cddbf
Shreeja Upadhyay <shreejaupadhyaycspitce@gmail.com>
2025-03-26 17:13:54 +0530

    Add files via upload

---
+++ l18.cpp
@@ -0,0 +1,16 @@
+//bitwise operators
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=2, b=3;
+
+    cout<<(a&b)<<endl;
+    cout<<(a|b)<<endl;
+    cout<<(a^b)<<endl;
+    cout<<(4<<1)<<endl;//left shift
+    cout<<(4>>1)<<endl;//right shift N
+
+}
