447988ec361f0fcf88ef901507b7fddbff0e2074
Shreeja Upadhyay <shreejaupadhyaycspitce@gmail.com>
2025-03-26 17:10:05 +0530

    Add files via upload

---
+++ l7.cpp
@@ -0,0 +1,25 @@
+//sum of  odd numbers of 1 to n numbers
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,sum=0;
+
+    cout<<"enter n";
+    cin>>n;
+
+    for(int i=1 ; i<=n ; i++)
+    {
+  if(i%2!=0)
+{
+    sum+=i;
+}
+
+
+    }
+
+     cout<<sum<<endl;
+
+}
