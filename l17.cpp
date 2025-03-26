bdd14dad4a0fdda596c129f1b55e869d2a290e7a
Shreeja Upadhyay <shreejaupadhyaycspitce@gmail.com>
2025-03-26 17:12:47 +0530

    Add files via upload

---
+++ l17.cpp
@@ -0,0 +1,25 @@
+//binary to decimal number
+
+#include<iostream>
+using namespace std;
+
+int binarytodecimal(int x)
+{
+    int ans=0 ,pow=1;
+
+    while(x>0)
+    {
+        int rem= x%10;
+        ans+=(rem *pow);
+        x/=10;
+        pow*=2;
+    }
+
+    return ans;
+}
+
+int main()
+{
+
+    cout<<binarytodecimal(1000);
+}
