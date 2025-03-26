28eb0fa6faa8a4d8daf8685d2998f0876cdc4d13
Shreeja Upadhyay <shreejaupadhyaycspitce@gmail.com>
2025-03-26 17:06:57 +0530

    Add files via upload

---
+++ l5.cpp
@@ -0,0 +1,17 @@
+//  ternary operator use
+
+// condition ? true : false ;
+
+
+#include<iostream>
+
+using namespace std;
+int main()
+{
+
+    int n;
+    cin>>n;
+
+    cout<< (n>=0?"positive" : "negative");
+}
+
