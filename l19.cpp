2a07ca11ba695fdcf1902e13a321703f0a97a57a
Shreeja Upadhyay <shreejaupadhyaycspitce@gmail.com>
2025-03-27 17:39:16 +0530

    Add files via upload

---
+++ l19.cpp
@@ -0,0 +1,46 @@
+//simple array
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    //by compiletime intialization
+    int marks[5]={1,2,3,4,5};
+
+    for(int i=0 ; i<5 ; i++)
+    {
+        cout<<marks[i]<<endl;
+    }
+
+
+
+    int n;
+    //by runtime intialization
+    cout<<endl<<"enter array size";
+    cin>>n;
+
+    int m[n];
+    cout<<"enter array elements:";
+      for(int i=0 ; i<n ; i++)
+    {
+        cin>>m[i];
+    }
+      for(int i=0 ; i<n ; i++)
+    {
+        cout<<m[i]<<endl;
+    }
+
+
+
+
+    //size of an array
+
+    cout<<"size of marks array:"<<sizeof(marks);
+
+
+
+    cout<<endl<<"elements present in marks array :"<<sizeof(marks)/sizeof(int);
+
+}
