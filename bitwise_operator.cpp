86e5a21733667a114597dff8a5a98dbe0892ae80
Shreeja Upadhyay <shreejaupadhyaycspitce@gmail.com>
2025-03-28 18:45:04 +0530

    Update bitwise_operator.cpp

--- bitwise_operator.cpp
+++ bitwise_operator.cpp
@@ -7,9 +7,10 @@ int main()
 {
     int a=2, b=3;
 
-    cout<<(a&b)<<endl;
-    cout<<(a|b)<<endl;
-    cout<<(a^b)<<endl;
+    cout<<(a&b)<<endl; //bitwise and
+    cout<<(a|b)<<endl;  //bitwise or
+
+    cout<<(a^b)<<endl; // bitwise exor
     cout<<(4<<1)<<endl;//left shift
     cout<<(4>>1)<<endl;//right shift N
 
